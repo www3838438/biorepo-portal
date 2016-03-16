f9cef12b16de0333a9f5c495b83281cb938a3083
Tyler Rivera <riverat2@email.chop.edu>
2016-03-16 12:43:57 -0400

    Linting

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,24 @@ var path = require('path');
 var ROOT_PATH = path.resolve(__dirname);
 
 module.exports = {
-    entry: path.resolve(ROOT_PATH,
-        'brp/static/embark/index'),
-    output: {
-        path: path.resolve(ROOT_PATH, 'brp/static/embark/build'),
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loaders: ['babel'],
-                exclude: /node_modules/,
-            }
-        ]
-    },
-    externals: {
-        //don't bundle the 'react' npm package with our bundle.js
-        //but get it from a global 'React' variable
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    }
+  entry: path.resolve(ROOT_PATH,
+      'brp/static/embark/index'),
+  output: {
+    path: path.resolve(ROOT_PATH, 'brp/static/embark/build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  externals: {
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
 };
