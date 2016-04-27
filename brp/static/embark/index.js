ce358fe61c585df759e198eb8127e528ab5b78f3
Tyler Rivera <riverat2@email.chop.edu>
2016-04-27 14:46:08 -0400

    eslint embark index

--- brp/static/embark/index.js
+++ brp/static/embark/index.js
@@ -3,28 +3,29 @@ import React from 'react';
 import App from './containers/App';
 import Navbar from './components/Navbar';
 import ProjectMenu from './components/ProjectMenu';
-import SubjectSelect  from './components/SubjectSelect';
-import SubjectView  from './components/SubjectView';
+import SubjectSelect from './components/SubjectSelect';
+import SubjectView from './components/SubjectView';
 import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { render } from 'react-dom';
 import configureStore from './store/configureStore';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
 
+/* eslint-disable no-underscore-dangle*/
 const store = configureStore(window.__INITIAL_STATE__);
 
 // Very manually constraining this to the root path until we establish proper
 // routing and views. This allows us to bounce out of the single page app paradigm
 // and into existing ehb-datasource url paths
-if (window.location.pathname == '/') {
+if (window.location.pathname === '/') {
   render(
     <Provider store={store}>
       <Router history={browserHistory}>
-        <Route path='/' component={App}>
+        <Route path="/" component={App}>
           <IndexRoute component={ProjectMenu} />
-          <Route path="dataentry/protocol/:id" component={SubjectSelect}/>
+          <Route path="dataentry/protocol/:id" component={SubjectSelect} />
           <Route
             path="dataentry/protocol/:prot_id/subject/:sub_id(/:edit)"
             component={SubjectView}
@@ -39,5 +40,5 @@ if (window.location.pathname == '/') {
     <Provider store={store}>
       <Navbar />
     </Provider>,
-  document.getElementById('react'))
+    document.getElementById('react'));
 }
