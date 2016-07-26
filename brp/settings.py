3aa26ef1e14c5623a24d9424c13028725e170fdc
Tyler Rivera <riverat2@email.chop.edu>
2016-07-26 12:48:58 -0400

    Make ebh logging configurable. Remove password validators in favor of LDAP convention

--- brp/settings.py
+++ brp/settings.py
@@ -111,20 +111,7 @@ AUTHENTICATION_BACKENDS = (
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+AUTH_PASSWORD_VALIDATORS = []
 
 # django auth user profile integration
 AUTH_PROFILE_MODULE = 'accounts.UserProfile'
@@ -223,7 +210,7 @@ LDAP['SERVER_URI'] = env('LDAP_SERVER_URI')
 LDAP['PREBINDPW'] = env('LDAP_PREBINDPW')
 
 # Enable Performance logging of 'ehb-client' logging facility
-EHB_LOG = True
+EHB_LOG = env.bool('EHB_LOG', True)
 
 # Logging Facilities
 LOGGING = {
