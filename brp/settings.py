8ac223bac7f3c57a98bfd8edb39c432d47c7dc42
Tyler Rivera <riverat2@email.chop.edu>
2016-10-10 13:39:13 -0400

    Example configuration to serve static files via nginx

--- brp/settings.py
+++ brp/settings.py
@@ -155,7 +155,10 @@ LOGIN_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = str(root.path('_site/static/'))
+if DEBUG:
+    STATIC_ROOT = str(root.path('_site/static/'))
+else:
+    STATIC_ROOT = '/opt/staticfiles/static/'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
