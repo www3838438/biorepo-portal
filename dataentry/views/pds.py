d623d9b8d14031b9e6884e28052e419191115651
huangs4 <huangs4@email.chop.edu>
2017-12-18 09:43:20 -0500

    set META action to errors so terminal outputs redcap error messages

--- dataentry/views/pds.py
+++ dataentry/views/pds.py
@@ -100,7 +100,7 @@ class FormView(DataEntryView):
         errors = self.driver.processForm(
             request=request, external_record=context['record'], form_spec=kwargs['form_spec'], session=request.session)
         if errors:
-            self.request.META['action'] = 'Errors processing form.'
+            self.request.META['action'] = errors
             self.request.META['error'] = True
             error_msgs = [e for e in errors]
             context['errors'] = error_msgs
