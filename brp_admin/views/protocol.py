e9a97af89f09a6c6b03c867a29fccde7add969c5
Sue Gerace <geraces@email.chop.edu>
2018-01-05 15:44:35 -0500

    adding capability to edit existing user credentials

--- brp_admin/views/protocol.py
+++ brp_admin/views/protocol.py
@@ -1,104 +1,155 @@
 from django.contrib.auth.models import User
-from api.models.protocols import ProtocolUser, ProtocolUserCredentials, Protocol, ProtocolDataSource
-
 from django.views.generic import TemplateView
-
-
 from django.shortcuts import render
+from django.forms.formsets import formset_factory
+from django.forms import modelformset_factory
+from django.core.exceptions import ObjectDoesNotExist
 
 from brp_admin.forms import ProtocolUserForm, ProtocolUserCredentialsForm
+from api.models.protocols import ProtocolUser, ProtocolUserCredentials, Protocol, ProtocolDataSource
 
-from django.forms.formsets import formset_factory
 
-class New_protocol_usr(TemplateView):
+class ProtocolUserView(TemplateView):
 
     template_name = 'new_protocol_usr.html'
 
-    def dispatch(self, request):
+    def __init__(self):
+        self.protocol_user_form = ProtocolUserForm()
 
-        credential_form_set = (formset_factory(ProtocolUserCredentialsForm, can_delete=True, extra=0))
+    def processProtocolUserForm(self, request):
 
-        if request.method == 'POST':
-            post = request.POST
+        post_info = request.POST
+        protocolUserForm = ProtocolUserForm(data=request.POST)
+        self.protocol = post_info['protocol']
+        self.user = post_info['user']
+        try:
+            self.protocol_user = ProtocolUser.objects.get(protocol=self.protocol, user=self.user)
+        except(ObjectDoesNotExist):
+            pass
 
-            protocol_user = 'protocol_user' in post
+        # if form is valid save
+        if protocolUserForm.is_valid():
+            protocolUserForm.save()
+            self.protocol_user = ProtocolUser.objects.get(protocol=post_info['protocol'], user=post_info['user'])
+        # to do: if errors send to UI
 
-            if 'protocol_user' in post or 'get_credentials' in post:
+    def get(self, request):
+        return render(request, 'new_protocol_usr.html', {'form1': ProtocolUserForm()})
 
-                protocolUserForm = ProtocolUserForm(data=request.POST)
+    def post(self, request):
+        post_data = request.POST
 
-                if protocolUserForm.is_valid():
-                    protocolUserForm.save()
-                    print("is userform valid?")
+        if 'submit_creds' in post_data:
+            return ProtocolUserCredentialForm.as_view()(self.request)
 
-                if not protocolUserForm.is_valid() and 'protocol_user' in post:
-                    context = {}
-                    context['errors'] = protocolUserForm.errors
-                    context['form1'] = protocolUserForm
-                    print("are we getting to else? here are errors:")
-                    print(context)
-                    return render(request, 'new_protocol_usr.html', context)
-                # get context data
-                context = {}
-                user = User.objects.get(pk=post['user'])
-                protocol = Protocol.objects.get(pk=post['protocol'])
-                context['user'] = user
-                context['protocol'] = protocol
-                empty_credentials = ProtocolDataSource.objects.filter(protocol=protocol)
-                protocol_user = ProtocolUser.objects.get(protocol=protocol, user=post['user'])
+        self.processProtocolUserForm(request)
 
-                if 'protocol_user' in post:
-                    credential_data = [{'protocol': protocol,
-                                        'data_source': item,
-                                        'user': user,
-                                        'protocol_user': protocol_user}
-                                       for item in empty_credentials]
-                if 'get_credentials' in post:
+        return ProtocolUserCredentialForm.as_view()(self.request)
 
-                    credentials = ProtocolUserCredentials.objects.filter(protocol=protocol, protocol_user=protocol_user)
-                    if not protocol_user:
-                        print("we are getting into error")
-                        context['error'] = " please add user to protocol"
-                        return render(request, 'new_protocol_usr.html', {'context': context})
-                    if not credentials:
-                        print("wer are getting into not credentials")
-                        credential_data = [{'protocol': protocol,
-                                            'data_source': item,
-                                            'user': user,
-                                            'protocol_user': protocol_user}
-                                           for item in empty_credentials]
-                    else:
-                        credential_data = [{'protocol': post['protocol'],
-                                            'protocol_user': protocol_user,
-                                            'user': user,
-                                            'data_source': cred.data_source,
-                                            'data_source_username': cred.data_source_username,
-                                            'data_source_password': cred.data_source_password}
-                                           for cred in credentials]
 
-                credential_form_set = (formset_factory(ProtocolUserCredentialsForm, can_delete=True, extra=0))
+class ProtocolUserCredentialForm(TemplateView):
 
-                credential_formSet = credential_form_set(initial=credential_data)
-                context['cred_formset'] = credential_formSet
-                return render(request, 'protocol_user_credentials.html', {'context': context})
+    def getCred_formset(self, protocol, protocol_user, user):
+        credentials = ''
+        try:
+            credentials = ProtocolUserCredentials.objects.filter(protocol=protocol, protocol_user=protocol_user)
+            credential_data = [{'protocol': protocol,
+                                'protocol_user': protocol_user,
+                                'user': user,
+                                'data_source': cred.data_source,
+                                'data_source_username': cred.data_source_username,
+                                'data_source_password': cred.data_source_password}
+                               for cred in credentials]
 
-            if 'submit_creds' in post:
-                context = {}
-                message = 'credentials not saved'
-                credentials = []
-                cred_formset = credential_form_set(post)
-                for cred_form in cred_formset:
-                    if cred_form.has_changed() and cred_form.is_valid():
-                        cred_form.save()
+        except(ObjectDoesNotExist):
+            pass
 
-                        context['data_source'] = cred_form
-                        message = 'credentials saved'
+        if (credentials):
+            credential_form_set = (modelformset_factory(
+                ProtocolUserCredentials,
+                fields='__all__',
+                can_delete=True,
+                extra=0))
+            self.credential_form_set = credential_form_set
 
-                context['message'] = message
-                return render(request, 'confirmation.html', context)
+            return credential_form_set(queryset=ProtocolUserCredentials.objects.filter(protocol=protocol, protocol_user=protocol_user))
+        else:
+            empty_credentials = ProtocolDataSource.objects.filter(protocol=protocol)
+            credential_data = [{'protocol': protocol,
+                                'data_source': item,
+                                'user': user,
+                                'protocol_user': protocol_user}
+                               for item in empty_credentials]
+            credential_form_set = (formset_factory(
+                ProtocolUserCredentialsForm,
+                can_delete=True,
+                extra=0))
+            # self.setCredentialFormSet(credential_form_set)
+            self.credential_form_set = credential_form_set
+            return credential_form_set(initial=credential_data)
 
-        protocolUserForm = ProtocolUserForm()
-        return render(request, 'new_protocol_usr.html', {'form1': protocolUserForm})
+    def getCredFormInstance(self, protocol_user, protocol, user, data_source):
+        try:
+            return ProtocolUserCredentials.objects.get(
+                protocol=protocol,
+                data_source=data_source,
+                user=user)
+        except(ObjectDoesNotExist):
+            pass
+        return ''
+
+    def processProtocolUserCredForm(self, request):
+        context = {}
+        context['message'] = 'credentials not saved'
+        cred_formset = self.credential_form_set(request.POST)
+        for cred_form in cred_formset:
+            if cred_form.has_changed() and cred_form.is_valid():
+                cred_form.save()
+
+
+                # get changes and send to confirmation page
+            # todo: if errors send to UI
+                context['data_source'] = cred_form
+                message = 'credentials saved'
+        print("changes: ")
+        print(self.credential_form_set.changed_objects())
+
+        context['message'] = message
+
+    def get_context_data(self, request):
+        request_info = request.POST
+        user = request_info['user']
+        protocol = request_info['protocol']
+        protocol_user = ProtocolUser.objects.get(protocol=protocol, user=user)
+
+        context = {}
+
+        context['cred_formset'] = self.getCred_formset(protocol,
+                                                       protocol_user,
+                                                       user)
+        context['user'] = User.objects.get(pk=user)
+        context['protocol'] = Protocol.objects.get(pk=protocol)
+        return context
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        post_data = request.POST
+
+        if 'submit_creds' in post_data:
+            user = post_data['form-0-user']
+            protocol = post_data['form-0-protocol']
+            protocol_user = ProtocolUser.objects.get(protocol=protocol, user=user)
+            self.getCred_formset(protocol, protocol_user, user)
+            self.processProtocolUserCredForm(request)
+            context = {}
+            context['message'] = "credentials saved"
+            return render(request, 'confirmation.html', context)
+
+        else:
+            context = self.get_context_data(request)
+            return render(request, 'protocol_user_credentials.html', context)
 
 
 class Fn_in_progress(TemplateView):
